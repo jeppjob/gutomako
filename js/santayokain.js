26a339bea382c296b4be47b6b05de9430e49b4c3
jeppjob <jepp.job@gmail.com>
2019-10-01 00:05:25 +0800

    custom fb button

--- js/santayokain.js
+++ js/santayokain.js
@@ -124,7 +124,13 @@ resetBtn.onclick = function() {
   showResult.style.display = "none";
 };
 
-// document.getElementById("submit").addEventListener("click", function() {
-//   let val = document.getElementById("userInput").value;
-//   userInput.push(val);
-// });
+let fbButton = document.getElementById('fb-share-button');
+let url = window.location.href;
+
+fbButton.addEventListener('click', function() {
+    window.open('https://www.facebook.com/sharer/sharer.php?u=' + url,
+        'facebook-share-dialog',
+        'width=800,height=600'
+    );
+    return false;
+});
